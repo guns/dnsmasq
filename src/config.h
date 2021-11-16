6e06c7dbcc21f366fdfec959c14e800254d695a0
guns <self@sungpae.com>
2021-11-15 22:27:40 -0600

    Restore HAVE_TFTP and HAVE_SCRIPT

--- src/config.h
+++ src/config.h
@@ -171,8 +171,8 @@ RESOLVFILE
 
 #define HAVE_DHCP
 #define HAVE_DHCP6 
-/* #define HAVE_TFTP */
-/* #define HAVE_SCRIPT */
+#define HAVE_TFTP
+#define HAVE_SCRIPT
 #define HAVE_AUTH
 #define HAVE_IPSET 
 #define HAVE_LOOP
